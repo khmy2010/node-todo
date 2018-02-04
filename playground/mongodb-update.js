caf1705d9ddb1e7ce2fe3b4b43ce4de29b43838f
khmy2010 <kehan.see@gmail.com>
2018-02-04 21:32:04 +0800

    updated script

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -7,5 +7,24 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
 
     const db = client.db('TodoApp');
 
-    // client.close();
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID("5a77018ffdcee014c5221f35")
+    // }, {
+    //     $set: { completed: true }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // })
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID("5a76d3ebef4d1dae4f9e9f3c")
+    }, {
+        $set: { name: 'see kehan' },
+        $inc: { age: 1 }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
 });
