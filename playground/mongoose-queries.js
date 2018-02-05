d9dc3f6e9ba551271d7321221c60754f179abdf4
khmy2010 <kehan.see@gmail.com>
2018-02-05 15:15:39 +0800

    fuck mongo.

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,28 @@
+var { mongoose } = require('../server/db/mongoose');
+var { User } = require('../server/models/user');
+var { Todo } = require('../server/models/todo');
+
+var id = '5a77eec34b6fe313848e8d73';
+console.log(Todo);
+
+//auto convert
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos: ', todos);
+// })
+
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo: ', todo);
+// });
+
+// var todo = new Todo({
+//     text: 'Cibai Mongo! Fuck!!!!!'
+// });
+
+// Todo.save().then((product) => {
+//     console.log('Product: ', product);
+// });
