a289dade39a13149a419c791f687b1d9805e8e7b
khmy2010 <kehan.see@gmail.com>
2018-02-05 10:10:28 +0800

    POST

--- server/server.js
+++ server/server.js
@@ -1,4 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var { mongoose } = require('./db/mongoose');
 var { User } = require('./models/user');
 var { Todo } = require('./models/todo');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(port, () => {
+    let date = new Date();
+
+    console.log(`${date.toTimeString()}: Server is listening at port ${port}.`);
+})
 
