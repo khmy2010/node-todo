c7ad02ccc1aee72f58d2a45e2f80497e6e22ba46
khmy2010 <kehan.see@gmail.com>
2018-02-07 23:24:09 +0800

    hashing.js

---
+++ playground/hashing.js
@@ -0,0 +1,36 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+    id: 10
+}
+
+var token = jwt.sign(data, 'ROCK');
+console.log(token.toString());
+
+var decodedResult = jwt.verify(token, 'ROCK');
+console.log('decoded: ', decodedResult);
+
+// var message = 'I AM GROOT!';
+// var hash = SHA256(message).toString();
+
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+// var data = {
+//     id: 4
+// };
+
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'SECRET').toString()
+// };
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'SECRET').toString();
+
+// if (resultHash === token.hash) {
+//     console.log('Data was original.');
+// }
+// else {
+//     console.log('Data was tampered.');
+// }
