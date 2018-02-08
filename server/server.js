ebd25ff11ba260a9f926c95b8810b427c70be7a6
khmy2010 <kehan.see@gmail.com>
2018-02-08 18:24:19 +0800

    add /login route

--- server/server.js
+++ server/server.js
@@ -114,9 +114,21 @@ app.post('/users', (req, res) => {
     })
 });
 
+app.post('/users/login', (req, res) => {
+    const {email, password} = _.pick(req.body, ['email', 'password']);
+    
+    User.findByCredentials(email, password).then((user) => {
+       return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
-})
+});
 
 app.listen(port, () => {
     let date = new Date();
